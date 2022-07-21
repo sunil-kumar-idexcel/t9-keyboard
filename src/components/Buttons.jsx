bec333c8c493f061ed9a3d20075a6be17c355d0d
Sunilkumar H <sunilkumar.h@idexcel.com>
2022-07-21 19:55:01 +0530

    commmit

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,21 +2,56 @@ import { Fragment, memo } from "react";
 import Debounce from "./Debounce";
 const Buttons = ({ setValue }) => {
   const chars = [
-    ["."],
-    ["a", "b", "c"],
-    ["d", "e", "f"],
-    ["g", "h", "i"],
-    ["j", "k", "l"],
-    ["m", "n", "o"],
-    ["p", "q", "r"],
-    ["s", "t", "u"],
-    ["v", "w", "x", "z"],
+    {
+      id: 1,
+      key_no: 1,
+      characters: ["."],
+    },
+    {
+      id: 2,
+      key_no: 2,
+      characters: ["a", "b", "c"],
+    },
+    {
+      id: 3,
+      key_no: 3,
+      characters: ["d", "e", "f"],
+    },
+    {
+      id: 4,
+      key_no: 4,
+      characters: ["g", "h", "i"],
+    },
+    {
+      id: 5,
+      key_no: 5,
+      characters: ["j", "k", "l"],
+    },
+    {
+      id: 6,
+      key_no: 6,
+      characters: ["m", "n", "o"],
+    },
+    {
+      id: 7,
+      key_no: 7,
+      characters: ["p", "q", "r"],
+    },
+    {
+      id: 8,
+      key_no: 8,
+      characters: ["s", "t", "u"],
+    },
+    {
+      id: 9,
+      key_no: 9,
+      characters: ["v", "w", "x", "z"],
+    },
   ];
-
   return (
     <Fragment>
       <div>
-        {[...Array(9)].map((e, index) => {
+        {chars.map((e, index) => {
           return (
             <button
               style={{
@@ -25,20 +60,20 @@ const Buttons = ({ setValue }) => {
                 margin: "5px",
                 width: "80px",
               }}
-              key={index}
+              key={e.id}
               onClick={Debounce(
-                function (chh) {
-                  console.log("in func", chh);
-                  setValue((prevState) => prevState + chh);
+                function (character) {
+                  console.log("in func", character);
+                  setValue((prevState) => prevState + character);
                 },
                 800,
-                chars[index],
+                e.characters,
                 setValue
               )}
             >
-              {index + 1}
+              {e.key_no}
               <br />
-              {chars[index]}
+              {e.characters}
             </button>
           );
         })}
